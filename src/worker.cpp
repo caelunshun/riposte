95521f3bb2569bb8df0ce03d5fe1065d553e1321
Caelum van Ispelen <caelum12321@gmail.com>
2021-05-20 12:01:38 -0600

    Forbid building on land that isn't your own

--- src/worker.cpp
+++ src/worker.cpp
@@ -36,6 +36,11 @@ namespace rip {
 
         const auto &unit = game.getUnit(unitID);
 
+        if (game.getCultureMap().getTileOwner(unit.getPos()) != unit.getOwner()) {
+            // You can only build on your own land....
+            return tasks;
+        }
+
         Cottage cottage(unit.getPos());
         tasks.push_back(std::make_unique<BuildImprovementTask>(cottage.getNumBuildTurns(), unit.getPos(), std::make_unique<Cottage>(std::move(cottage))));
         Mine mine(unit.getPos());
