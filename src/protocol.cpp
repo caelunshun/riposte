895ffd5b38c63620e5de21cdf040d9346f8a9edc
caelunshun <caelunshun@gmail.com>
2021-06-14 23:10:41 -0600

    Initial protocol for multiplayer

---
+++ src/protocol.cpp
@@ -0,0 +1,5 @@
+//
+// Created by caelum on 6/14/21.
+//
+
+#include "protocol.h"
---
+++ src/protocol.h
@@ -0,0 +1,8 @@
+//
+// Created by caelum on 6/14/21.
+//
+
+#ifndef RIPOSTE_PROTOCOL_H
+#define RIPOSTE_PROTOCOL_H
+
+#endif //RIPOSTE_PROTOCOL_H
