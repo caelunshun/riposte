7eb1fa17e7693d0fb6932cab246c31a0f063434a
caelunshun <caelunshun@gmail.com>
2021-06-14 23:11:18 -0600

    Delete C++ protocol files (Multiplayer, switching to protobufs)

--- src/protocol.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by caelum on 6/14/21.
-//
-
-#include "protocol.h"
--- src/protocol.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by caelum on 6/14/21.
-//
-
-#ifndef RIPOSTE_PROTOCOL_H
-#define RIPOSTE_PROTOCOL_H
-
-#endif //RIPOSTE_PROTOCOL_H
