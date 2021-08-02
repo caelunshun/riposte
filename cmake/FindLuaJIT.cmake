616334c8b14a90cd3e5da0c957defe408bedaa1e
Caelum van Ispelen <caelunshun@gmail.com>
2021-08-02 16:02:33 -0600

    Update FindLuaJIT.cmake

--- cmake/FindLuaJIT.cmake
+++ cmake/FindLuaJIT.cmake
@@ -11,28 +11,28 @@ endif()
 
 set(LUAJIT_DEFINITIONS ${PC_LUAJIT_CFLAGS_OTHER})
 
-find_path(LUAJIT_INCLUDE_DIR luajit.h
+find_path(LuaJIT_INCLUDE_DIR luajit.h
           PATHS ${PC_LUAJIT_INCLUDEDIR} ${PC_LUAJIT_INCLUDE_DIRS}
           PATH_SUFFIXES luajit-2.0 luajit-2.1)
 
 if(MSVC)
-  list(APPEND LUAJIT_NAMES lua51)
+  list(APPEND LuaJIT_NAMES lua51)
 elseif(MINGW)
-  list(APPEND LUAJIT_NAMES libluajit libluajit-5.1)
+  list(APPEND LuaJIT_NAMES libluajit libluajit-5.1)
 else()
-  list(APPEND LUAJIT_NAMES luajit-5.1)
+  list(APPEND LuaJIT_NAMES luajit-5.1)
 endif()
 
-find_library(LUAJIT_LIBRARY NAMES ${LUAJIT_NAMES}
+find_library(LuaJIT_LIBRARY NAMES ${LuaJIT_NAMES}
              PATHS ${PC_LUAJIT_LIBDIR} ${PC_LUAJIT_LIBRARY_DIRS})
 
-set(LuaJIT_LIBRARY ${LUAJIT_LIBRARY})
-set(LuaJIT_INCLUDE_DIR ${LUAJIT_INCLUDE_DIR})
+set(LuaJIT_LIBRARY ${LuaJIT_LIBRARY})
+set(LuaJIT_INCLUDE_DIR ${LuaJIT_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LUAJIT_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(LuaJit DEFAULT_MSG
-                                  LUAJIT_LIBRARY LUAJIT_INCLUDE_DIR)
+                                  LuaJIT_LIBRARY LuaJIT_INCLUDE_DIR)
 
-mark_as_advanced(LUAJIT_INCLUDE_DIR LUAJIT_LIBRARY)
+mark_as_advanced(LuaJIT_INCLUDE_DIR LuaJIT_LIBRARY)
