3259c6810783cabaf13b5a7baa70bf47aeb9d0d4
Caelum van Ispelen <caelum12321@gmail.com>
2021-09-11 18:08:20 -0600

    Use relative path for protobuf build script

--- client/protocol/build.rs
+++ client/protocol/build.rs
@@ -2,8 +2,8 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     prost_build::compile_protos(
-        &["/Users/caelum/CLionProjects/riposte/proto/riposte.proto"],
-        &["/Users/caelum/CLionProjects/riposte/proto/"],
+        &["../../proto/riposte.proto"],
+        &["../../proto/"],
     )?;
     Ok(())
 }
