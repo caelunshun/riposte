efc8edf5616b9592d7bb10317eea53d5ef6e1ebb
caelunshun <caelunshun@gmail.com>
2021-12-24 13:32:32 -0700

    Add texture variants for rivers

--- crates/client/src/renderer/rivers.rs
+++ crates/client/src/renderer/rivers.rs
@@ -1,30 +1,39 @@
 use std::f32::consts::TAU;
 
 use dume::TextureId;
-use glam::{UVec2, vec2};
+use glam::{vec2, UVec2};
+use rand::{Rng, SeedableRng};
+use rand_pcg::Pcg64Mcg;
 use riposte_common::{river::Axis, Tile};
 
 use crate::{context::Context, game::Game};
 
 use super::TileRenderLayer;
 
-pub struct RiverRenderer {
-    straight: TextureId,
+struct Segment {
+    texture: TextureId,
     offset: f32,
+}
+
+fn segment(cx: &Context, id: &str) -> Segment {
+    let texture = cx.canvas().context().texture_for_name(id).unwrap();
+    let dims = cx.canvas().context().texture_dimensions(texture);
+    let offset = (dims.y as f32 / dims.x as f32) * 100. / 2.;
+    Segment { texture, offset }
+}
+
+pub struct RiverRenderer {
+    straights: Vec<Segment>,
 }
 
 impl RiverRenderer {
     pub fn new(cx: &Context) -> Self {
-        let straight =  cx
-        .canvas()
-        .context()
-        .texture_for_name("texture/river/straight_0")
-        .unwrap();
-        let straight_dims = cx.canvas().context().texture_dimensions(straight);
-        Self {
-            straight,
-            offset: (straight_dims.y as f32 / straight_dims.x as f32) * 100. / 2.,
-        }
+        let straights = vec![
+            segment(cx, "texture/river/straight_0"),
+            segment(cx, "texture/river/straight_1"),
+            segment(cx, "texture/river/straight_2"),
+        ];
+        Self { straights }
     }
 }
 
@@ -36,7 +45,12 @@ impl TileRenderLayer for RiverRenderer {
                 if axis == Axis::Vertical {
                     canvas.rotate(TAU / 4.);
                 }
-                canvas.draw_sprite(self.straight, vec2(0., -self.offset), 100.);
+
+                let mut rng =
+                    Pcg64Mcg::seed_from_u64(((tile_pos.x as u64) << 32) | tile_pos.y as u64);
+                let segment = &self.straights[rng.gen_range(0..self.straights.len())];
+
+                canvas.draw_sprite(segment.texture, vec2(0., -segment.offset), 100.);
                 if axis == Axis::Vertical {
                     canvas.rotate(-TAU / 4.);
                 }
